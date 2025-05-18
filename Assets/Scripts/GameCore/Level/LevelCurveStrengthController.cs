400a318892f4d4c0d3f9872ff67c3f4cffb517de
Aleksandr Litskevich <avlitskevich@gmail.com>
2025-05-18 17:42:45 +0500

    Started adding curved floor

---
+++ Assets/Scripts/GameCore/Level/LevelCurveStrengthController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace GameCore.Level
+{
+    [ExecuteAlways]
+    public class LevelCurveStrengthController : MonoBehaviour
+    {
+        private static readonly int _curveStrengthId = Shader.PropertyToID("_CurveStrength");
+
+        [SerializeField] private bool _enableCurve;
+        [SerializeField] [Range(-0.03f, 0.03f)] private float _curveStrength;
+
+        private void Update()
+        {
+            float strength = _enableCurve ? _curveStrength : 0f;
+            Shader.SetGlobalFloat(_curveStrengthId, strength);
+        }
+    }
+}
